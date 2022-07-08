3c5d93f16bb79a4d461e8473273022244acb7e9c
Markus Wibisono <bloming61@gmail.com>
2022-07-08 22:58:05 +0700

    update
    Signed-off-by: Markus Wibisono <bloming61@gmail.com>

--- src/Routes.js
+++ src/Routes.js
@@ -3,56 +3,20 @@
 import {
   BrowserRouter as Router,
   Route,
-  HashRouter,
-  Switch,
-  Link
+  Switch
+
 } from "react-router-dom";
 
 
-import HomePage from './Template/HomePage';
 
-import NavbarPage from './Template/NavbarPage';
-
-
-import AboutPage from './Template/AboutPage';
-
-import TeamsPage from './Template/TeamsPage';
-
-import ContactPage from './Template/ContactPage';
-
-import PortofolioPage from './Template/PortofolioPage';
-
-
-  export default function Routes() {
-    return (
-      <Router>
-         <Switch>
-            <Route exact path="/testing-hosting">
-                <NavbarPage flagMenu="home"/>
-                <HomePage />
-                <AboutPage />
-                <TeamsPage />
-                <PortofolioPage />
-                <ContactPage /> 
-             </Route>
-             <Route exact path="/testing-hosting/about">
-                < NavbarPage flagMenu="about"/>
-                <AboutPage />
-             </Route>
-             <Route exact path="/testing-hosting/team">
-                < NavbarPage flagMenu="team" />
-                <TeamsPage /> 
-             </Route>
-             <Route exact path="/testing-hosting/portofolio">
-                < NavbarPage flagMenu="portofolio" />
-                <PortofolioPage/>
-             </Route>
-             <Route exact path="/testing-hosting/contact">
-                < NavbarPage flagMenu="contact" />
-                <ContactPage/>
-             </Route>
-         </Switch>
-      </Router>
-    );
-  }
-  
+export default function Routes() {
+  return (
+    <Router>
+       <Switch>
+          <Route exact path="/testdomain">
+                testing
+           </Route>
+       </Switch>
+    </Router>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-
+import App from './App';
 
 import Routes from './Routes';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Routes />
+     <Routes/>
   </React.StrictMode>
 );
 
