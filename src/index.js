1a6a340a66a4143d9b102f82005e6a9dcca480d7
Markus Wibisono <bloming61@gmail.com>
2022-07-08 23:28:27 +0700

    update
    Signed-off-by: Markus Wibisono <bloming61@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-import App from './App';
+import Routes from './Routes';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <App/>
+      <Routes />
   </React.StrictMode>
 );
 
