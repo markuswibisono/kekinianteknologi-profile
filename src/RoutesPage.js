ad621ce9e79075d97100066650a411896ff38bc0
Markus Wibisono <bloming61@gmail.com>
2022-07-09 23:54:31 +0700

    update menu
    Signed-off-by: Markus Wibisono <bloming61@gmail.com>

--- src/RoutesPage.js
+++ src/RoutesPage.js
@@ -1,15 +1,36 @@
 
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
+import HomePage from './Template/HomePage';
+
+import NavbarPage from './Template/NavbarPage';
+
+
+import AboutPage from './Template/AboutPage';
+
+import TeamsPage from './Template/TeamsPage';
+
+import ContactPage from './Template/ContactPage';
+
+import PortofolioPage from './Template/PortofolioPage';
+
+
+
 export default function RoutesPage() {
   return (
     <Router basename={"/testing-hosting"}>
       <Switch>
-        <Route exact path='/about'>
-          page about
+        <Route exact path='/'>
+            <NavbarPage flagMenu="home"/>
+            <HomePage />
+            <AboutPage />
+            <TeamsPage />
+            <PortofolioPage />
+            <ContactPage />  
         </Route>
-        <Route exact path='/test'>
-          page test
+        <Route exact path="/about">
+                < NavbarPage flagMenu="about"/>
+                <AboutPage />
         </Route>
       </Switch>      
     </Router>
