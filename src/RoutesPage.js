b4ddff6691c9782cef93beb48f53c4179096067f
Markus Wibisono <bloming61@gmail.com>
2022-07-09 23:16:17 +0700

    update
    Signed-off-by: Markus Wibisono <bloming61@gmail.com>

--- src/RoutesPage.js
+++ src/RoutesPage.js
@@ -4,14 +4,11 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 export default function RoutesPage() {
   return (
     <Router basename={"/testing-hosting"}>
-    <Switch>
-      <Route exact path='/about'>
+      <Switch>
+        <Route exact path='/about'>
           page about
-      </Route>
-      <Route exact path='/profile'>
-          page profile
-      </Route>
-    </Switch>      
-</Router>
+        </Route>
+      </Switch>      
+    </Router>
   );
 }
